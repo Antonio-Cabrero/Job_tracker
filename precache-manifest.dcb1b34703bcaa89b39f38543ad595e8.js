d02db49f95fc05e46da8cdc4d074e74dcf8b5fb4
ZealousSloth <49397161+ZealousSloth@users.noreply.github.com>
2019-11-14 18:27:23 +0100

    Updates

---
+++ precache-manifest.dcb1b34703bcaa89b39f38543ad595e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17f79872d2c8c66ad08a81da3874011",
+    "url": "/Job_tracker/index.html"
+  },
+  {
+    "revision": "4ed4aa6b63b1d47debec",
+    "url": "/Job_tracker/static/css/main.9c7248ef.chunk.css"
+  },
+  {
+    "revision": "889e5985e873aad54868",
+    "url": "/Job_tracker/static/js/2.17919689.chunk.js"
+  },
+  {
+    "revision": "4ed4aa6b63b1d47debec",
+    "url": "/Job_tracker/static/js/main.b6ebe50f.chunk.js"
+  },
+  {
+    "revision": "3dec0c270da77828fe19",
+    "url": "/Job_tracker/static/js/runtime-main.fd474cd6.js"
+  }
+]);
